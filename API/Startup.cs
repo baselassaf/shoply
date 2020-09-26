55972fa878a1fc6e4035d0b4bbc6da9d28009a92
Basel <basel.assafff@gmail.com>
2020-09-26 12:18:49 +0300

    API Archi

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Core.Interfaces;
 
 namespace API {
     public class Startup {
@@ -23,7 +24,9 @@ namespace API {
         //public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
+        public void ConfigureServices (IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers ();
             services.AddDbContext<StoreContext> (x => 
             x.UseSqlite (_configuration.GetConnectionString("DefaultConnection")));
