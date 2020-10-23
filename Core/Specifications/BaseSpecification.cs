e1f1c952088d68e45c65b358e3953fe29cfbafe2
Basel <basel.assafff@gmail.com>
2020-10-23 23:31:55 +0300

    API paging, filtering and searching

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -30,6 +30,7 @@ namespace Core.Specifications
 
         public Expression<Func<T, object>> OrderByDescending {get; private set;}
 
+
         protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
         {
             OrderBy = orderByExpression;
@@ -38,6 +39,19 @@ namespace Core.Specifications
         protected void AddOrderByDescending(Expression<Func<T, object>> orderByDscExpression)
         {
             OrderByDescending = orderByDscExpression;
+        }
+
+        public int Take {get; private set;}
+
+        public int Skip {get; private set;}
+
+        public bool IsPagingEnabled {get; private set;}
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip= skip;
+            Take = take;
+            IsPagingEnabled = true;
         }
 
     }
--- Infrastructure/GenericRepository.cs
+++ Infrastructure/GenericRepository.cs
@@ -7,18 +7,23 @@ using Core.Specifications;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure {
-    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity {
+namespace Infrastructure
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
         private readonly StoreContext _context;
-        public GenericRepository (StoreContext context) {
+        public GenericRepository(StoreContext context)
+        {
             _context = context;
         }
 
-        public async Task<T> GetByIdAsync (int id) {
+        public async Task<T> GetByIdAsync(int id)
+        {
             return await _context.Set<T>().FindAsync(id);
         }
 
-        public async Task<IReadOnlyList<T>> ListAllAsync () {
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
             return await _context.Set<T>().ToListAsync();
         }
 
@@ -32,9 +37,16 @@ namespace Infrastructure {
             return await ApplySpecification(spec).ToListAsync();
         }
 
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
-            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);           
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
+
+
     }
 }
